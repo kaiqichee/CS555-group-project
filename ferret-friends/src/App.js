c59374415fa3b66600ceeca59334f49a18b357d2
Burak-Yesil <burakyesilinvestment@gmail.com>
2022-02-28 17:19:54 -0500

    Added lower health button and isAlive method

--- ferret-friends/src/App.js
+++ ferret-friends/src/App.js
@@ -1,18 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [health, setHealth] = useState(1)
+
   return (
     <div className="App" style={{ height: '100%', position: 'absolute', left: '0px', width: '100%', overflow: 'hidden' }}>
       <h1 style={{ color: "white" }}>Welcome to Garden Builder</h1>
       <br />
       <div className="increment-screen">
-        <button style={{ height: 50, backgroundColor: "#6eeb34", borderWidth: 7, borderRadius: 5 }} className="water-plant"> Water Plant </button>
+        <button style={{ height: 50, backgroundColor: "#6eeb34", borderWidth: 7, borderRadius: 5, borderColor: "#E36959" }} className="water-plant"
+          onClick={() => setHealth(health + 1)}> Water Plant </button>
         <br />
-        <text></text>
+        <h2 style={{ color: "white" }}>Current Health: {health}</h2>
+
+        <button style={{ height: 50, backgroundColor: "red" }} className="deprive-plant"
+          onClick={() => setHealth(health - 1)}> lower health (not for production/for testing) </button>
       </div>
     </div>
   );
 }
+
+
+
 
 export default App;
---
+++ ferret-friends/src/plantStatus.js
@@ -0,0 +1,6 @@
+//This library should contain functions to learn more about plants health status
+
+
+function isAlive(healthLevel) {
+    return (healthLevel <= 0) ? false : true;
+}
