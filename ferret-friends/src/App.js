559c3b29f96d48390361bd066c2162e8e7fae679
lukasasz <55508557+lukasasz@users.noreply.github.com>
2022-03-01 13:04:39 -0500

    la 2 user stories done, with todo added

--- ferret-friends/src/App.js
+++ ferret-friends/src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [health, setHealth] = useState(1);
+  const [health, setHealth] = useState(1); // TODO: make sure plant can't be watered unless water_level >= 0
+  const [seeds, setSeeds] = useState(0);
+  const [water_level, collectWater] = useState(0);
   const value = 10;
   return (
     <div
@@ -46,8 +48,46 @@ function App() {
           {" "}
           Water Plant{" "}
         </button>
+      <br/>
+      <br/>
+        <button
+          style={{
+            height: 50,
+            backgroundColor: "#63db34",
+            borderWidth: 7,
+            borderRadius: 5,
+            borderColor: "#E36851",
+          }}
+          className="buy-seeds"
+          onClick={() => setSeeds(seeds + 1)}
+        >
+          {" "}
+          Buy Seeds{" "}
+        </button>
+        <br/>
+        <br/>
+        <button
+          style={{
+            height: 50,
+            backgroundColor: "#6eeb34",
+            borderWidth: 7,
+            borderRadius: 5,
+            borderColor: "#E36959",
+          }}
+          className="collect-water"
+          onClick={() => collectWater(water_level + 1)}
+        >
+          {" "}
+          Collect Water{" "}
+        </button>
+      <br/>
+      <br/>
         <br />
         <h2 style={{ color: "white" }}>Current Health: {health}</h2>
+        <br/>
+        <h2 style={{ color: "white" }}>Number of Seeds: {seeds}</h2>
+        <br/>
+        <h2 style={{ color: "white" }}>Water Level: {water_level}</h2>
 
         <button
           style={{ height: 50, backgroundColor: "red" }}
