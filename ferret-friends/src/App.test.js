5ac8bde47464af25182c76ce751548ae173fa804
MaxSurets <maxims2019@gmail.com>
2022-03-01 19:45:25 -0500

    Add tests for checking fertilizer and water amount

--- ferret-friends/src/App.test.js
+++ ferret-friends/src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {
+  createUser,
+  checkWater,
+  checkFertilizer,
+} from "../db/users";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("water amount", async () => {
+  let user = await createUser("name");
+  let waterAmount = await checkWater(user)
+  expect(waterAmount).toBe(0);
+});
+
+test("fertilizer amount", async () => {
+  let user = await createUser("name");
+  let fertilizerAmount = await checkFertilizer(user)
+  expect(fertilizerAmount).toBe(0);
 });
