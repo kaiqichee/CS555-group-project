916821bb6c1daad49b11e0f638ff4d7c7ad91dc8
Burak-Yesil <burakyesilinvestment@gmail.com>
2022-03-01 19:39:39 -0500

    Added cmments to App.test.js

--- ferret-friends/src/App.test.js
+++ ferret-friends/src/App.test.js
@@ -6,3 +6,6 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+//No way to test the buttons because it isn't connected to the database yet
