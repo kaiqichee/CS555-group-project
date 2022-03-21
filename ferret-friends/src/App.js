3fa77c90f79df4cb104b1ae5fbf6aef661fba3ac
Darshil Shah <darshilshah1899@gmail.com>
2022-03-21 19:23:27 -0400

    Grow fruit tree
    When a user has grown enough plants, he will be able to grow a fruit tree

--- ferret-friends/src/App.js
+++ ferret-friends/src/App.js
@@ -1,12 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
 function App() {
   const [health, setHealth] = useState(1); // TODO: make sure plant can't be watered unless water_level >= 0
   const [seeds, setSeeds] = useState(0);
+  const [fruit, setFruits] = useState(0);
   const [water_level, collectWater] = useState(0);
   const value = 10;
+
   return (
     <div
       className="App"
@@ -20,10 +21,10 @@ function App() {
     >
       <h1 style={{ color: "white" }}>Welcome to Garden Builder</h1>
       <br />
-
       {health > value && (
         <h2 style={{ color: "Red" }}>Total plant: {(health - 1) / 10} </h2>
       )}
+
       {health >= value && <h2>Plant is fully grown</h2>}
       {health >= value && (
         <h2>
@@ -32,7 +33,6 @@ function App() {
           what those events mean{" "}
         </h2>
       )}
-
       <div className="increment-screen">
         <button
           style={{
@@ -48,6 +48,23 @@ function App() {
           {" "}
           Water Plant{" "}
         </button>
+        <br />
+        {(health - 1) / 10 >= 5 && (
+          <button
+            style={{
+              height: 50,
+              backgroundColor: "#6eeb34",
+              borderWidth: 7,
+              borderRadius: 5,
+              borderColor: "#E36959",
+            }}
+            className="grow-fruit"
+            onClick={() => setFruits(fruit + 1)}
+          >
+            {" "}
+            Grow fruit tree{" "}
+          </button>
+        )}
         <br />
         <button
           style={{ height: 50, backgroundColor: "red" }}
@@ -87,13 +104,10 @@ function App() {
           {" "}
           Collect Water{" "}
         </button>
-
-        <br />
-        <h2 style={{ color: "white" }}>Current Health: {health}</h2>
-        <br />
-        <h2 style={{ color: "white" }}>Number of Seeds: {seeds}</h2>
-        <br />
-        <h2 style={{ color: "white" }}>Water Level: {water_level}</h2>
+        <h4 style={{ color: "white" }}>Current Health: {health}</h4>
+        <h4 style={{ color: "white" }}>Fruit Plants: {fruit}</h4>
+        <h4 style={{ color: "white" }}>Number of Seeds: {seeds}</h4>
+        <h4 style={{ color: "white" }}>Water Level: {water_level}</h4>
       </div>
     </div>
   );
