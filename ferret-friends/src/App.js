1b55ed724a2d3335527e0ad0f197af82a480b5ec
Darshil Shah <darshilshah1899@gmail.com>
2022-03-21 17:39:00 -0400

    UI change which was causing conflicts
    UI change which was causing conflicts

--- ferret-friends/src/App.js
+++ ferret-friends/src/App.js
@@ -49,6 +49,14 @@ function App() {
           Water Plant{" "}
         </button>
         <br />
+        <button
+          style={{ height: 50, backgroundColor: "red" }}
+          className="deprive-plant"
+          onClick={() => setHealth(health - 1)}
+        >
+          {" "}
+          lower health (not for production/for testing){" "}
+        </button>
         <br />
         <button
           style={{
@@ -65,7 +73,6 @@ function App() {
           Buy Seeds{" "}
         </button>
         <br />
-        <br />
         <button
           style={{
             height: 50,
@@ -80,23 +87,13 @@ function App() {
           {" "}
           Collect Water{" "}
         </button>
-        <br />
-        <br />
+
         <br />
         <h2 style={{ color: "white" }}>Current Health: {health}</h2>
         <br />
         <h2 style={{ color: "white" }}>Number of Seeds: {seeds}</h2>
         <br />
         <h2 style={{ color: "white" }}>Water Level: {water_level}</h2>
-
-        <button
-          style={{ height: 50, backgroundColor: "red" }}
-          className="deprive-plant"
-          onClick={() => setHealth(health - 1)}
-        >
-          {" "}
-          lower health (not for production/for testing){" "}
-        </button>
       </div>
     </div>
   );
