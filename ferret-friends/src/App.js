6347ff9cf0301a97c02e950d33804c0cbec5c735
lukasasz <55508557+lukasasz@users.noreply.github.com>
2022-03-22 12:18:57 -0400

    background color and big plant user stories done

--- ferret-friends/src/App.js
+++ ferret-friends/src/App.js
@@ -9,10 +9,40 @@ function App() {
   const [health, setHealth] = useState(1); // TODO: make sure plant can't be watered unless water_level >= 0
   const [seeds, setSeeds] = useState(0);
   const [water_level, setWater] = useState(0);
-
+  const [bg_color, setBgColor] = useState(0);
 
   const SmallSizeLimit = 50;
   const MediumSizeLimit = 200;
+
+  var colors = [
+    {
+      value: 1,
+      label: "teal"
+    },
+    {
+      value: 2,
+      label: "green"
+    },
+    {
+      value: 3,
+      label: "gray"
+    },
+    {
+      value: 4,
+      label: "#51ABF0"
+    }
+  ];
+  var [inputValue, setInputValue] = useState(colors[0].label);
+  var [bgcolor, setbgColor] = useState(colors[0].label);
+
+  var ddlhandle = (e) => {
+    setInputValue(e.target.value);
+  }
+
+  var buttonHandle = () => {
+    setBgColor(inputValue);
+  }
+
 
   function returnSize(health) {
     //Returns the current size of the plant
@@ -20,6 +50,9 @@ function App() {
       return "Small"
     } else if (health <= MediumSizeLimit) {
       return "Medium"
+    }
+    else {
+      return "Big"
     }
   }
 
@@ -34,6 +67,7 @@ function App() {
   }
 
 
+
   return (
     <div
       className="App"
@@ -43,10 +77,30 @@ function App() {
         left: "0px",
         width: "100%",
         overflow: "hidden",
+        backgroundColor: bg_color
       }}
     >
+      <div>
+        <button 
+        onClick={buttonHandle}
+        style={{
+        display: 'flex', 
+        justifyContent: 'left', 
+        height: 35,
+        backGroundcolor: "teal",
+        borderWidth: 7,
+        borderRadius: 5,
+        borderColor: "#E36959"
+        }}>{" "} Change Background Color {" "}</button>
+        <select style = {{display: 'flex', justifyContent: 'left'}} onChange={ddlhandle}>
+        {colors.map((color) => (
+          <option value={color.label}>{color.label}</option>
+        ))}
+      </select>
+      </div>
       <h1 style={{ color: "white" }}>Welcome to Garden Builder</h1>
       <br />
+
 
       <div className="increment-screen">
         <button
